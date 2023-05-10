51f73c15e93d090ec4e70bfe5df46a7f8876e7f6
ayotech-py <ajaomalik2018@gmail.com>
2023-05-10 16:57:16 +0000

    rotate 2D matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''3D matrix
+'''
+
+
+def rotate_2d_matrix(matrix):
+    """The function"""
+    rep_matrix = []
+    for i in range(3):
+        sub_matrix = []
+        for j in range(3):
+            itr = 3 - (j + 1)
+            sub_matrix.append(matrix[itr][i])
+        rep_matrix.append(sub_matrix)
+    for i in range(3):
+        matrix[i] = rep_matrix[i]
